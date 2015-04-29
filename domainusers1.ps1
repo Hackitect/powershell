4231354ec4c303299d46dd5748aa66b2efa368fe
Hackitect <Hackitect@users.noreply.github.com>
2015-04-29 11:22:00 +0300

    Update domainusers1.ps1

--- domainusers1.ps1
+++ domainusers1.ps1
@@ -2,16 +2,8 @@
 #writen by Charles M. Njenga MIS Officer Danish Refugee Council
 #Modified 29-04-2015
 
-# The variables which change per location are domain name which are; these are held in the variable name $HOA-Domain
-#ddgsom.org
-#drcdjibouti.org
-#drcethiopia.org
-#drchoa.org
-#drckenya.org
-#drcsomalia.org
-#drcyemen.org
-#ddghoa.org
-#ddgsomalia.org 
+# The variables which change per location are domain name which are; these are held in the variable name $domainarray
+# Just some functions to play with date
 $a = (get-date).day
 $a = (get-date).dayofweek
 $a = (get-date).dayofyear
@@ -24,14 +16,15 @@ $a = (get-date).timeofday
 $a = (get-date).year
 #initialize domain arrays
 #$Fieldstofetch = @(Country, DisplayName, UserPrincipalName, Title, Department, State, Office, PhoneNumber, MobilePhone, LastPasswordChangeTimestamp, FirstName, LastName)
-$array = @("drckenya.org", "drchoa.org", "drcsomalia.org", "drcethiopia.org") #you can add more domains here
+$array = @("drckenya.org", "drchoa.org", "drcsomalia.org", "drcethiopia.org", "ddghoa.org", "ddgsom.org", "ddgyemen.org", "drcdjibouti.org", "drcyemen.org", "regionalmms.org") #you can add more domains here
 
-#$DomainHOA[7] = "ddgyemen.org"
+#$DomainHOA[7] = "ddgyemen.org" /removed this type of declaration for domains
 
 #Construct path to save the output
 $myDate = (date).tostring('dd')+"-"+(date).tostring('yyyy')
 $FileName2Exportall = "$env:USERPROFILE\Desktop\drckenya.all"+$myDate+".csv"
 
+#to work on interesting ways to strip the .org part from domain when saving the file - 
 #Sign in to get authenticated
 $LiveCred = Get-Credential
 $Session = New-PSSession -ConfigurationName Microsoft.Exchange -ConnectionUri https://ps.outlook.com/powershell/ -Credential $LiveCred -Authentication Basic -AllowRedirection
@@ -43,7 +36,8 @@ Connect-MSolservice
 #Get specific fields about the users which are the most important
 
 
-
+#this will will retrieve all the domains in the array. if you want to restrict to only 1 domain comment the below
+# 4 lines
 foreach ($element in $array) { 
         $FileName2Export = "$env:USERPROFILE\Desktop\$element"+$myDate+".csv"
         Get-MsolUser -DomainName $element | Select-Object Country, DisplayName, FirstName, LastName, UserPrincipalName, Title, Department, State, Office, PhoneNumber, MobilePhone, LastPasswordChangeTimestamp  | Export-Csv $FileName2Export
